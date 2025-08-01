c6992bb8456a360af3ef90ee9ed16b5f19606c8f
AlchemistChief <meistergamer333@gmail.com>
2025-08-01 22:24:59 +0200

    Added Coloring Function

--- server/functions/utils.ts
+++ server/functions/utils.ts
@@ -1,3 +1,6 @@
 // ────────── Module Importing ──────────
 // ────────── Custom Modules ──────────
 // ────────── Utils Module (Backend) ──────────
+export function colorizeANSCII(text: string, colorCode: number): string {
+    return `\x1b[${colorCode}m${text}\x1b[0m`;
+}
