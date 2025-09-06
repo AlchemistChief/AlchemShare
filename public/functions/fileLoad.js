ac1eacf2cec4c52c12facf51a5ebcb39cbdb13ba
AlchemistChief <meistergamer333@gmail.com>
2025-09-06 21:14:01 +0200

    Placeholder Function

--- public/functions/fileLoad.js
+++ public/functions/fileLoad.js
@@ -1,4 +1,11 @@
 // functions/fileLoad.js
 
 // ────────── Custom Modules ──────────
-import { logClientMessage } from "./functions/utils.js";
+import { logClientMessage } from "./utils.js";
+import { sendAPIRequest } from "./apiHandler.js";
+
+export async function initFileLoad() {
+    logClientMessage("INFO", "[FILE] Loading files...");
+    const response = await sendAPIRequest({ method: "GET", endpoint: "/listfiles" });
+    const data = await response.json();
+}
