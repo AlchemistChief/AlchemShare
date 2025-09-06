ad687b00a949dfc294e1e77b5e983e1803bd9e44
AlchemistChief <meistergamer333@gmail.com>
2025-09-06 21:07:16 +0200

    added "status" to error

--- server/assets/globals.ts
+++
@@ -1,5 +0,0 @@
-// ────────── Module Importing ──────────
-import path from 'path';
-
-// ────────── Globals Exporting ──────────
-export const FILE_DIR = path.join(import.meta.dirname, '../files');
