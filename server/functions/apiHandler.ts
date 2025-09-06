80777936f4b9291a30766a62f3667981b67205df
AlchemistChief <meistergamer333@gmail.com>
2025-09-06 21:12:52 +0200

    Improved API Handler

--- server/functions/apiHandler.ts
+++ server/functions/apiHandler.ts
@@ -1,5 +1,5 @@
 // ────────── Module Importing ──────────
-import type { Request, Response, RequestHandler } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 
 // ────────── Custom Modules ──────────
 import { loginCheck } from './loginCheck.ts';
@@ -7,29 +7,35 @@ import { getFileList } from './fileLoad.ts';
 
 // ────────── API Modules ──────────
 
-export function apiHandler(req: Request<{ endpoint: string }>, res: Response): void {
+export function apiHandler(req: Request<{ endpoint: string }>, res: Response, next: NextFunction): void {
     const endpoint = req.params.endpoint;
     let authToken
 
-    // Skip auth for login endpoint
-    if (endpoint !== 'login') {
-        authToken = req.headers['authorization'] || '';
-        if (!authToken) {
-            res.status(401).json({ error: 'Missing or invalid authorization token' });
-            return;
-        }
-    }
+    try {
 
-    // ────────── Endpoint Routing ──────────
-    switch (endpoint) {
-        case 'login':
-            loginCheck(req, res);
-            break;
-        case 'listfiles':
-            getFileList(req, res, authToken);
-            break;
-        default:
-            res.status(404).json({ error: `API endpoint '${endpoint}' not found.` });
-            return;
+        // Skip auth for login endpoint
+        if (endpoint !== 'login') {
+            authToken = req.headers['authorization'] || '';
+            if (!authToken) {
+                const err = new Error('Missing or invalid authorization token');
+                err.status = 401;
+                throw err;
+            }
+        }
+
+        // ────────── Endpoint Routing ──────────
+        switch (endpoint) {
+            case 'login':
+                loginCheck(req, res);
+                break;
+            case 'listfiles':
+                getFileList(req, res, authToken);
+                break;
+            default:
+                res.status(404).json({ error: `API endpoint '${endpoint}' not found.` });
+                return;
+        }
+    } catch (err) {
+        next(err);
     }
 }
