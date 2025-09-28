7f76a59774474844fa019b952de306e1bba45d0e
AlchemistChief <meistergamer333@gmail.com>
2025-09-28 14:24:29 +0200

    Removed Error Specific Files

--- public/errors/errors.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const params = new URLSearchParams(window.location.search);
-    const errorMsg = params.get('errorMessage');
-    if (errorMsg) {
-        const el = document.getElementById('error-message');
-        if (el) el.textContent = `Error: ${errorMsg}`;
-    }
-});
